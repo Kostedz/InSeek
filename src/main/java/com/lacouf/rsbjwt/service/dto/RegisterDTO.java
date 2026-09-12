4a5e3db4c2fa18381392c9998d90c85a5fa39bf5
kostedz <u5719661@gmail.com>
2026-09-12 18:19:17 -0400

    Modification du type de l'attribut 'role' dans RegisterDTO pour utiliser l'énumération Role

--- src/main/java/com/lacouf/rsbjwt/service/dto/RegisterDTO.java
+++ src/main/java/com/lacouf/rsbjwt/service/dto/RegisterDTO.java
@@ -1,6 +1,7 @@
 package com.lacouf.rsbjwt.service.dto;
 
 import com.lacouf.rsbjwt.model.Disciplines;
+import com.lacouf.rsbjwt.model.auth.Role;
 
-public record RegisterDTO(String nom, String prenom, String email, String role, String password, Disciplines discipline) {
+public record RegisterDTO(String nom, String prenom, String email, Role role, String password, Disciplines discipline) {
 }
