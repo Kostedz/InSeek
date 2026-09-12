cac60963dce1df2bd6ed36748053bd96c47be710
kostedz <u5719661@gmail.com>
2026-09-12 18:19:27 -0400

    Suppression des imports inutilisés dans UtilisateurDTO

--- src/main/java/com/lacouf/rsbjwt/service/dto/UtilisateurDTO.java
+++ src/main/java/com/lacouf/rsbjwt/service/dto/UtilisateurDTO.java
@@ -1,11 +1,5 @@
 package com.lacouf.rsbjwt.service.dto;
 
-import com.lacouf.rsbjwt.model.Utilisateur;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 public interface UtilisateurDTO {
     String nom();
     String prenom();
