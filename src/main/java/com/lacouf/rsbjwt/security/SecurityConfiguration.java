c4c4fb3966cdfbda0e655137634a1a0e4f2741ce
kostedz <u5719661@gmail.com>
2026-09-12 18:19:23 -0400

    Suppression de l'importation de la classe Etudiant dans SecurityConfiguration

--- src/main/java/com/lacouf/rsbjwt/security/SecurityConfiguration.java
+++ src/main/java/com/lacouf/rsbjwt/security/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.lacouf.rsbjwt.security;
 
-import com.lacouf.rsbjwt.model.Etudiant;
 import com.lacouf.rsbjwt.model.auth.Role;
 import com.lacouf.rsbjwt.repository.UserAppRepository;
 import lombok.RequiredArgsConstructor;
