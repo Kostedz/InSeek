1fd284aa8b9d0d18b9e185060c8b0bd1b6f44039
kostedz <u5719661@gmail.com>
2026-09-12 17:50:39 -0400

    Ajout du service UtilisateurService pour la gestion des étudiants et ajout de la méthode findByEmail dans UtilisateurRepository

---
+++ src/main/java/com/lacouf/rsbjwt/service/UtilisateurService.java
@@ -0,0 +1,62 @@
+package com.lacouf.rsbjwt.service;
+
+import com.lacouf.rsbjwt.exception.BadRequestException;
+import com.lacouf.rsbjwt.model.Etudiant;
+import com.lacouf.rsbjwt.model.Utilisateur;
+import com.lacouf.rsbjwt.repository.UtilisateurRepository;
+import com.lacouf.rsbjwt.security.JwtTokenProvider;
+import com.lacouf.rsbjwt.service.dto.EtudiantDTO;
+import com.lacouf.rsbjwt.service.dto.RegisterDTO;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class UtilisateurService {
+    private final AuthenticationManager authenticationManager;
+    private final UtilisateurRepository utilisateurRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public EtudiantDTO ajouterEtudiant(RegisterDTO utilisateurDTO) throws BadRequestException {
+        if (utilisateurDTO == null) {
+            throw new BadRequestException("Le DTO de l'utilisateur est null.");
+        }
+        Etudiant user = (Etudiant) toEntity(utilisateurDTO);
+        utilisateurRepository.save(user);
+        return EtudiantDTO.of(user);
+    }
+
+    @Transactional
+    public EtudiantDTO findEtudiantByEmail(String email) throws BadRequestException {
+        if (email == null || email.isBlank()) {
+            throw new BadRequestException("L'email ne peut pas être null ou vide.");
+        }
+
+        Etudiant etudiant = (Etudiant) utilisateurRepository.findByEmail(email);
+        return EtudiantDTO.of(etudiant);
+    }
+
+    public Utilisateur toEntity(RegisterDTO registerDTO) throws BadRequestException {
+        if (registerDTO == null) {
+            return null;
+        }
+
+        if (registerDTO.role().equals("ETUDIANT")) {
+            return Etudiant.builder()
+                    .nom(registerDTO.nom())
+                    .prenom(registerDTO.prenom())
+                    .email(registerDTO.email())
+                    .discipline(registerDTO.discipline())
+                    .password(passwordEncoder.encode(registerDTO.password()))
+                    .build();
+        }
+
+        throw new BadRequestException("Type de DTO non pris en charge pour la conversion en entité.");
+    }
+}
