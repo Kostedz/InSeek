90f6c9fb5ef8d2c536ec8cf24fc80c6192b1d8d8
kostedz <u5719661@gmail.com>
2026-09-12 17:49:22 -0400

    Ajout de gestion des exceptions personnalisées pour les erreurs de requête, conflit et non trouvé

---
+++ src/main/java/com/lacouf/rsbjwt/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package com.lacouf.rsbjwt.exception;
+
+public class ConflictException extends Exception {
+  public ConflictException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/com/lacouf/rsbjwt/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.lacouf.rsbjwt.exception;
+
+public class NotFoundException extends Exception {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
