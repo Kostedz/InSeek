50ea2db29dafe9666c34c383355782beabea09a0
Telimiumx5 <6297475@claurendeau.qc.ca>
2026-09-12 15:03:54 -0400

    Ajout du dto Professeur

---
+++ src/main/java/com/lacouf/rsbjwt/service/dto/ProfesseurDto.java
@@ -0,0 +1,30 @@
+package com.lacouf.rsbjwt.service.dto;
+
+import com.lacouf.rsbjwt.model.Professeur;
+import com.lacouf.rsbjwt.model.auth.Role;
+import lombok.Builder;
+
+public class ProfesseurDto extends UserDTO{
+
+    @Builder
+    public ProfesseurDto(Long id, String firstName, String lastname, String email, Role role) {
+        super(id, firstName, lastname, email, role);
+    }
+
+    public ProfesseurDto() {}
+
+    public static ProfesseurDto create(Professeur professeur) {
+        return ProfesseurDto.builder()
+                .id(professeur.getId())
+                .firstName(professeur.getFirstName())
+                .lastname(professeur.getLastName())
+                .email(professeur.getEmail())
+                .role(professeur.getRole())
+                .build();
+    }
+
+    public static ProfesseurDto empty() {
+        return new ProfesseurDto();
+    }
+
+}
