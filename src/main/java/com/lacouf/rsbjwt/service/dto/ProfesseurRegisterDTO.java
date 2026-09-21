304a75089a57f33f9f91d2e66735457c3651452f
Telimiumx5 <6297475@claurendeau.qc.ca>
2026-09-21 16:28:45 -0400

    Ãcorrection du sync role

--- src/main/java/com/lacouf/rsbjwt/service/dto/ProfesseurRegisterDTO.java
+++ src/main/java/com/lacouf/rsbjwt/service/dto/ProfesseurRegisterDTO.java
@@ -1,6 +1,7 @@
 package com.lacouf.rsbjwt.service.dto;
 
 import com.lacouf.rsbjwt.model.Disciplines;
+import com.lacouf.rsbjwt.model.auth.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ public class ProfesseurRegisterDTO {
     private String lastName;
     private String email;
     private String password;
+    Role role;
     private String affilitation;
-    private Disciplines discipline;
 
 }
